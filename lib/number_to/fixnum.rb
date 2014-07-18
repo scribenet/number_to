0b8ccba0c32ef22e68bd70f2d823aaf030b4972f
dcorrigan <dcorrigan@scribenet.com>
2014-07-18 11:19:16 -0400

    has monkeypatch file for fixnum

---
+++ lib/number_to/fixnum.rb
@@ -0,0 +1,24 @@
+class Fixnum
+  def to_upper_roman
+    NumberTo.to_upper_roman(self)
+  end
+  def to_lower_roman
+    NumberTo.to_lower_roman(self)
+  end
+  def to_upper_alpha
+    NumberTo.to_upper_alpha(self)
+  end
+  def to_lower_alpha
+    NumberTo.to_lower_alpha(self)
+  end
+  def to_ordinal
+    NumberTo.to_ordinal(self)
+  end
+  def to_words(options = {})
+    NumberTo.to_words(self, options)
+  end
+  def to_word_ordinal(options = {})
+    NumberTo.to_word_ordinal(self, options)
+  end
+end
+
